ae761efc0a694ea6a61eef073bbed877562b5863
RaraV2 <87941142+RaraV2@users.noreply.github.com>
2026-06-14 20:44:42 +0530

    Add files via upload

---
+++ server.cc
@@ -0,0 +1,24 @@
+#include "../HTTP_client_in_cpp/cmd/httpserver/httpserver.hpp"
+
+constexpr uint16_t PORT{42069};
+
+int main() {
+
+  sigset_t set;
+  sigemptyset(&set);
+  sigaddset(&set, SIGINT);
+
+  // Block SIGINT delivery so sigwait can catch it instead
+  pthread_sigmask(SIG_BLOCK, &set, nullptr);
+
+  HTTP_Server Server;
+  Server.Serve(PORT, Handler);
+  Server.Listen();
+
+  int sig;
+  // This line completely blocks execution until Ctrl+C is pressed
+  sigwait(&set, &sig);
+  Server.Close();
+
+  return 0;
+}
